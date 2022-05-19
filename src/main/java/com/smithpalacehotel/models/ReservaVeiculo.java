4076f479167cb334bd7a72990fa7ec6a87b633e2
André Rodrigues Correia <75909599+Andre-Rodrigues-Correia@users.noreply.github.com>
2022-05-19 15:26:56 -0300

    create class ReservaVeiculo

---
+++ src/main/java/com/smithpalacehotel/models/ReservaVeiculo.java
@@ -0,0 +1,26 @@
+package com.smithpalacehotel.models;
+
+import java.io.Serializable;
+import javax.validation.constraints.*;
+import java.time.LocalDateTime;
+import javax.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = {"id"})
+public class ReservaVeiculo implements Serializable{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Integer id;
+
+    @NotBlank(message = "A data deve ser preenchida.")
+    public LocalDateTime data;
+
+    @NotBlank(message = "O status deve ser preenchido.")
+    public Boolean status;
+  
+  //referenciar reservaQuarto
+}

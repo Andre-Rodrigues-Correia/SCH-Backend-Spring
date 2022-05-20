635dc83aed6f1a49159d471783fc0626879eb3d7
Jones Santos Brito <85516594+JonesBrito@users.noreply.github.com>
2022-05-20 19:58:14 -0300

    Create CheckOut

---
+++ src/main/java/com/smithpalacehotel/models/CheckOut.java
@@ -0,0 +1,25 @@
+package com.smithpalacehotel.models;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import javax.validation.constraints.*;
+import javax.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = {"id"})
+public class CheckOut {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Integer id;
+
+    @NotBlank(message = "A data deve ser preenchida.")
+    public LocalDateTime dataCheckout;
+
+    @OneToOne
+    @JoinColumn(name="checkin_id")
+    public CheckIn checkin;
+}

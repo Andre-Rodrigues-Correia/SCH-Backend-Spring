abda0391f442f66124427b6212f96e9ff7810b8d
William Desteffani Soares <86719742+WilliamDS-Uchiha@users.noreply.github.com>
2022-06-25 20:27:40 -0300

    Implementação das Regras de Negócio do Local de Evento
    Implementação das Regras de Negócio do Local de Evento

---
+++ src/main/java/com/smithpalacehotel/sch/services/ReservaEventoService.java
@@ -0,0 +1,95 @@
+package com.smithpalacehotel.sch.services;
+
+import java.util.Collection;
+import java.util.NoSuchElementException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
+
+import com.smithpalacehotel.sch.models.ReservaEvento;
+import com.smithpalacehotel.sch.repository.ReservaEventoRepository;
+import com.smithpalacehotel.sch.services.exceptions.DataIntegrityException;
+import com.smithpalacehotel.sch.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ReservaEventoService {
+
+    @Autowired
+    private ReservaEventoRepository repository;
+
+    public ReservaEvento findById(Integer id) {
+    	try {
+        	ReservaEvento obj = repository.findById(id).get();
+        	return obj;
+        } catch (NoSuchElementException e) {
+        	throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + ReservaEvento.class.getName());
+        }
+    }
+
+    public ReservaEvento insert(ReservaEvento obj) {
+        obj.setId(null);
+        try {
+        	return repository.save(obj);
+        } catch (DataIntegrityViolationException e) {
+            throw new DataIntegrityException("Campo(s) obrigatório(s) do Local de Evento não foi(foram) preenchido(s): Nome, Capacidade");
+        }
+    }
+
+    public ReservaEvento update(ReservaEvento obj) {
+    	findById(obj.getId());
+        try {
+        	return repository.save(obj);
+        } catch (DataIntegrityViolationException e) {
+            throw new DataIntegrityException("Campo(s) obrigatório(s) da Local de Evento não foi(foram) preenchido(s): Nome, Capacidade");
+        }
+    }
+
+    public void delete(Integer id) {
+        findById(id);
+        try {
+            repository.deleteById(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DataIntegrityException("Não é possível excluir um Local de Evento associado a reservas!");
+        }
+    }
+
+    public Collection<ReservaEvento> findAll() {
+        return repository.findAll();
+    }
+
+    public boolean verificarRegrasDeNegocio(ReservaEvento obj){
+        // Regra de Negócio 1: Impedir o aluguel de um local de evento mais de 3 vezes seguidas em um mesmo mês por uma mesma pessoa.
+        boolean aluguelInvalido = false;
+        
+        Collection<ReservaEvento> reservas = repository.findLastReservaEventoByCliente(obj.getCliente().getId());
+
+        Year ano = obj.getData().getYear();
+        Month mes = obj.getData().getMonth();
+        int num = 0;
+
+        for (ReservaEvento reserva : reservas)
+            if (reserva.getData().getMonth() == mes && reserva.getData().getYear() == ano) num++;
+
+        if (num >= 2) aluguelInvalido = true;
+
+        // Regra de Negocio 2: Verificar se a quantidade máxima de pessoas foi atingida no local de evento.
+        boolean capacidadeMaxima = false;
+
+        if (repository.findCapacidadeByReservaEvento(obj.getId())){
+            capacidadeMaxima = true;
+        }
+
+        if (!capacidadeMaxima){
+            throw new BusinessRuleException("Capacidade máxima do local excedida!");
+        }
+
+        if (capacidadeMaxima && aluguelInvalido){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+}
